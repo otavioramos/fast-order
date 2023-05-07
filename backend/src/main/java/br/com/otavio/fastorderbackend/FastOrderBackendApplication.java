af4988ef7f9009ade7d6d0d51da9041fd6d95bac
Otavio Ramos <otavio.omak@gmail.com>
2023-05-07 14:01:24 -0300

    Creating backend project

---
+++ backend/src/main/java/br/com/otavio/fastorderbackend/FastOrderBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.otavio.fastorderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastOrderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastOrderBackendApplication.class, args);
+	}
+
+}
