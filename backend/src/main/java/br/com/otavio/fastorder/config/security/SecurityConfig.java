ff23c41b134753e7b5cacc71484009faa3b250a7
Otavio Ramos <otavio.omak@gmail.com>
2023-07-30 18:19:17 -0300

    Alterando SecurityConfig por conta da depreciação do WebSecurityConfigurerAdapter

--- backend/src/main/java/br/com/otavio/fastorder/config/security/SecurityConfig.java
+++ backend/src/main/java/br/com/otavio/fastorder/config/security/SecurityConfig.java
@@ -1,21 +1,20 @@
 package br.com.otavio.fastorder.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter
-{
-    @Override
-    protected void configure(HttpSecurity http) throws Exception
-    {
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-         .csrf().disable()
-         .authorizeRequests().anyRequest().authenticated()
-         .and()
-         .httpBasic();
+                .authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        return http.csrf().disable().build();
     }
 }
