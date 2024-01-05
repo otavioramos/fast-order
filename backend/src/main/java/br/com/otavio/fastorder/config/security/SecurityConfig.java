7249007a69005133a850b73d8bfb95eaf03e220a
otavio.santana <otavio.santana@pd.tec.br>
2024-01-05 11:24:33 -0300

    Liberando Swagger no SecurityConfig

--- backend/src/main/java/br/com/otavio/fastorder/config/security/SecurityConfig.java
+++ backend/src/main/java/br/com/otavio/fastorder/config/security/SecurityConfig.java
@@ -5,16 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
-                .httpBasic(withDefaults());
-        return http.csrf().disable().build();
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/swagger-ui/*").permitAll()
+                .antMatchers("/api-docs", "/api-docs/*").permitAll()
+                .antMatchers("/actuator", "/actuator/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
     }
 }
